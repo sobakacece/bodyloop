372aa67256d256df59cccc4616773b60f5e953f5
sobakacece <daren.d.d11@gmail.com>
2025-08-12 23:30:58 +0300

    Updated physics and fall state

--- Assets/Scripts/PlayerNormal.cs
+++ Assets/Scripts/PlayerNormal.cs
@@ -8,6 +8,8 @@ using UnityEngine;
 public class PlayerNormal : PlayerState
 {
     // Start is called before the first frame update
+    [SerializeField]
+    private float movingSpeed = 5.0f;
     public override void OnEnter()
     {
         //player.hands.GetComponent<MeshRenderer>().enabled = false;
@@ -18,7 +20,7 @@ public class PlayerNormal : PlayerState
         player.MyCurrentStamina += Time.deltaTime * player.staminaRecoverySpeed;
         if (Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.D))
         {
-            player.Move();
+            player.Move(movingSpeed);
         }
 
         if (player.CouldStartClimb() && (Input.GetMouseButton(0) || Input.GetMouseButton(1)) && !player.staminaDepleted)
@@ -26,6 +28,18 @@ public class PlayerNormal : PlayerState
             stateMachine.ChangeState(StateMachine.StateEnum.Climb);
         }
 
+        if (!player.IsGrounded())
+        {
+            stateMachine.ChangeState(StateMachine.StateEnum.Fall);
+
+        }
+
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space) && player.IsGrounded())
+            player.Jump(player.jumpHeight);
     }
 
 }
--- README.md
+++ README.md
@@ -1,6 +1,8 @@
 //TODO
         // Add curves to acceleration/deacceleration - LOW - transitioned to low, as I added lerps and they are ok
         // Add random movement at high points - LOW
-        // Add fall state - HIGH
         // Refactor inputs (drop events, lol) - LOW
         // Fix bug quick snapping while holding input and releasing hand - HIGH
+        
+        // NEXT: fix a level and update it
+        // concept corpse moving mechanics
